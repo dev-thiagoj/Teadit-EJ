00e9aa445125b8c6dcc1bfbe7b452bb6358cd4d6
Thiago de Castro <dev.thiagoj@gmail.com>
2026-02-27 10:14:35 -0300

    feat: prefab pantografica

--- Assets/Scripts/Camera/CADCameraController.cs
+++ Assets/Scripts/Camera/CADCameraController.cs
@@ -165,6 +165,8 @@ public class CADCameraController : MonoBehaviour
     }
     private void OnExplosionChanged(float arg0)
     {
+        return;
+
         if (modelRoot == null)
             return;
 
